b3f48f0d059d6f555b17b6923a594c45fbab0b2a
EthanPaleyron <epaleyron@edenschool.fr>
2023-11-03 15:04:08 +0100

    acceuil fini

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("opened");
   menu.setAttribute("aria-expanded", menu.classList.contains("opened"));
 });
+
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+function handleMediaQueryChange(mediaQuery) {
+  if (mediaQuery.matches) {
+    lists.style.display = "flex";
+    menu.classList.add("opened");
+  }
+}
+handleMediaQueryChange(mediaQuery);
+mediaQuery.addListener(handleMediaQueryChange);
+
 const carrousel = document.querySelector("#carrousel");
 const titleCarrousel = document.querySelector("#titleCarrousel");
 const description = document.querySelector("#description");
