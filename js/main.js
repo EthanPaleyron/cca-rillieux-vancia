592d925cc6c912705876737ca4261b1689c64d16
EthanPaleyron <epaleyron@edenschool.fr>
2023-11-09 22:42:44 +0100

    fin du aside

--- js/main.js
+++ js/main.js
@@ -9,15 +9,54 @@ menu.addEventListener("click", () => {
   menu.classList.toggle("opened");
   menu.setAttribute("aria-expanded", menu.classList.contains("opened"));
 });
-const mediaQuery = window.matchMedia("(min-width: 768px)");
-function handleMediaQueryChange(mediaQuery) {
-  if (mediaQuery.matches) {
+const aside = document.querySelector("aside");
+let moveSizeAside;
+let itClosed = false;
+function mediaQueryAside() {
+  if (window.matchMedia("(min-width: 1040px)").matches) {
+    moveSizeAside = 19;
+  } else {
+    moveSizeAside = 15;
+  }
+}
+document.querySelector("#closeAside").addEventListener("click", () => {
+  mediaQueryAside();
+  itClosed = true;
+  aside.style.transform = `translateX(${moveSizeAside}rem)`;
+});
+document.querySelector("#openAside").addEventListener("click", () => {
+  mediaQueryAside();
+  if (itClosed) {
+    aside.style.transform = "translateX(0)";
+    itClosed = false;
+  } else {
+    aside.style.transform = `translateX(${moveSizeAside}rem)`;
+    itClosed = true;
+  }
+});
+const mediaQuery = addEventListener("resize", () => {
+  if (window.matchMedia("(min-width: 768px)").matches) {
     lists.style.display = "flex";
     menu.classList.add("opened");
   } else {
     lists.style.display = "none";
     menu.classList.remove("opened");
   }
+  if (window.matchMedia("(min-width: 1040px)").matches && itClosed == true) {
+    aside.style.transform = `translateX(19rem)`;
+  } else if (
+    !window.matchMedia("(min-width: 1040px)").matches &&
+    itClosed == true
+  ) {
+    aside.style.transform = `translateX(15rem)`;
+  }
+});
+let i = parseInt(localStorage.getItem("newPerson"));
+if (!i) {
+  localStorage.setItem("newPerson", 1);
+  aside.style.transform = "translateX(0)";
+} else {
+  mediaQueryAside();
+  aside.style.transform = `translateX(${moveSizeAside}rem)`;
 }
-handleMediaQueryChange(mediaQuery);
-mediaQuery.addListener(handleMediaQueryChange);
+console.log(localStorage.getItem("newPerson"));
