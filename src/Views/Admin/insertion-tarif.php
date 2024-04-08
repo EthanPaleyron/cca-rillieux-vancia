03a76db8284e8af8819023b6f2a471e5fd9d613f
EthanPaleyron <epaleyron@edenschool.fr>
2024-04-08 21:15:51 +0000

    fin du old + des keywords

--- src/Views/Admin/insertion-tarif.php
+++ src/Views/Admin/insertion-tarif.php
@@ -7,19 +7,23 @@ ob_start();
     <div class="content">
         <h2>Creation d'un nouveau tarif</h2>
         <form action="/newTarif/" method="post" enctype="multipart/form-data">
-            <input type="text" name="nom_tarif" id="nom_tarif" placeholder="Nom du tarif" autocomplete="off">
+            <input type="text" name="nom_tarif" id="nom_tarif" placeholder="Nom du tarif" autocomplete="off"
+                value="<?= old("nom_tarif") ?>">
             <label for="nom_tarif" class="error">
                 <?= error("nom_tarif") ?>
             </label>
-            <input type="number" name="premier_chien" id="premier_chien" placeholder="Tarif du premier chien">
+            <input type="number" name="premier_chien" id="premier_chien" placeholder="Tarif du premier chien"
+                value="<?= old("premier_chien") ?>">
             <label for="premier_chien" class="error">
                 <?= error("premier_chien") ?>
             </label>
-            <input type="number" name="deuxieme_chien" id="deuxieme_chien" placeholder="Tarif du deuxieme chien">
+            <input type="number" name="deuxieme_chien" id="deuxieme_chien" placeholder="Tarif du deuxieme chien"
+                value="<?= old("deuxieme_chien") ?>">
             <label for="deuxieme_chien" class="error">
                 <?= error("deuxieme_chien") ?>
             </label>
-            <input type="number" name="par_chien" id="par_chien" placeholder="Tarif par chien">
+            <input type="number" name="par_chien" id="par_chien" placeholder="Tarif par chien"
+                value="<?= old("par_chien") ?>">
             <label for="par_chien" class="error">
                 <?= error("par_chien") ?>
             </label>
@@ -28,7 +32,7 @@ ob_start();
                 <select name="ordre_tarif" id="ordre_tarif">
                     <option value="0">En premier</option>
                     <?php foreach ($tarifs as $key => $tarif) {
-                        if ($tarif->getordre_tarif() + 1 === sizeof($tarifs)) { // Si c'est le dernier de la liste on le select            ?>
+                        if ($tarif->getordre_tarif() + 1 === sizeof($tarifs)) { // Si c'est le dernier de la liste on le select             ?>
                             <option selected value="<?= $tarif->getordre_tarif() + 1 ?>">
                                 Après
                                 <?= $tarif->getnom_tarif() ?>
